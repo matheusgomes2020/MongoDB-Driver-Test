de4ae564de192ab45920fbfbbb8d2253cba50611
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-04-25 12:26:39 -0300

    crud operations

--- app.js
+++ app.js
@@ -8,26 +8,33 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/FruitsDB2');
  
   const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please ckeck your data entry, no name specified!"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
   });
  
   const Fruit = mongoose.model('Fruit', fruitSchema);
  
   const fruit = new Fruit({
-    name: 'Apple',
-    rating: 7,
+    //name: 'Apple',
+    rating: 10,
     review: 'Pretty solid as a fruit.'
   });
  
   //await fruit.save();
  
-  const fruits = await Fruit.find();
-  fruits.forEach(function(fruit){
-    console.log(fruit.name)
-    mongoose.connection.close();
-  });
+  //const fruits = await Fruit.find();
+  //fruits.forEach(function(fruit){
+  //  console.log(fruit.name)
+   // mongoose.connection.close();
+  //});
   //console.log(fruits);
  
   const personSchema = new mongoose.Schema({
@@ -36,13 +43,30 @@ async function main() {
   });
  
   const Person = mongoose.model('Person', personSchema);
- 
-  const person = new Person({
-    name: 'John',
-    age: 37
+
+    const mango = new Fruit({
+    name: 'Mango',
+    score: 3,
+    review: 'The best fruit!'
   });
+
+  await mango.save(); 
+
+   Person.updateOne({name: "John"}, {favouriteFruit: mango})
+    .then(() => {
+   console.log("Successfully Updated");
+ })
+ .catch((err) => {
+   console.log(err);
+ });
+
+  //const person = new Person({
+   // name: 'Amy',
+   // age: 12,
+   // favouriteFruit: pineapple
+  //});
  
-  //await person.save();
+ // await person.save();
  
   const people = await Person.find();
   console.log(people);
@@ -74,5 +98,43 @@ async function main() {
 //  .catch(function (err) {
  //   console.log(err);
  // });
+
+ //Fruit.updateOne({_id: "6447e6d94cadb54b7c85a37e"}, {name: "Peach"})
+ //.then(() => {
+ //  console.log("Successfully Updated");
+ //})
+ //.catch((err) => {
+ //  console.log(err);
+ //});
+
+// Fruit.deleteOne({ name: "Peach" })
+ //.then(() => {
+ //  console.log("Successfully Deleted");
+ //})
+// .catch((err) => {
+//   console.log(err);
+ //});
+
+// Person.deleteMany({name: "John"})
+//.then(() => {
+  //console.log("Successfully Deleted All Data named JOHN");
+//})
+//.catch((err) => {
+  //console.log(err);
+//});
+
+//READ ALL ENTRIES IN COLLECTION
+Fruit.find()
+.then((fruits) => {
+    fruits.forEach(fruit => {
+        console.log(fruit)
+    })
+})
+.catch(err => {
+    console.log(err)
+})
+.finally(() => mongoose.connection.close())
+
+
  
 } 
