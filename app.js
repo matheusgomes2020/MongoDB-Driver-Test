35f75edb6b258bba40c33e58b4ba091dc763ce07
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-04-25 11:54:03 -0300

    Db working

--- app.js
+++ app.js
@@ -1,50 +1,78 @@
-const {MongoClient} = require("mongodb");
+const mongoose = require('mongoose');
  
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = 'mongodb://127.0.0.1:27017';
+main().catch(err => console.log(err));
  
-// const client = new MongoClient(uri, {useUnifiedTopology: true});
-const client = new MongoClient(uri);
-async function run() {
-    try {
-        await client.connect();
-        console.log("Connected Successfully to server");
+async function main() {
+  // Use connect method to connect to the server
+ // mongoose.set('strictQuery', false);
+  await mongoose.connect('mongodb://127.0.0.1:27017/FruitsDB2');
  
-        const database = client.db('fruitsDB');
-        const fruitsCollection = database.collection('fruits');
-        const docs = [
-            {
-              name: "Lichi",
-              score: 8,
-              review: "Great Fruit",
-            },
-            {
-              name: "Pineapple",
-              score: 6,
-              review: "Kind Sour",
-            },
-            {
-              name: "Mango",
-              score: 9,
-              review: "Great Stuff!",
-            },
-          ]
-        const option={ordered: true};
-        const result=await fruitsCollection.insertMany(docs,option);
-        const cursor = fruitsCollection.find({});
-        console.log(`${result.insertedCount} documents were inserted`);
-        if ((await fruitsCollection.countDocuments) === 0) {
-            console.log("No documents found!");
-        }
-        console.log(`Total entries ${await fruitsCollection.countDocuments()}`);
-        await cursor.forEach((fruit) => {
-            console.log(fruit);
-        });
+  const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+  });
  
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
+  const Fruit = mongoose.model('Fruit', fruitSchema);
  
-run().catch(console.dir);
+  const fruit = new Fruit({
+    name: 'Apple',
+    rating: 7,
+    review: 'Pretty solid as a fruit.'
+  });
+ 
+  //await fruit.save();
+ 
+  const fruits = await Fruit.find();
+  fruits.forEach(function(fruit){
+    console.log(fruit.name)
+    mongoose.connection.close();
+  });
+  //console.log(fruits);
+ 
+  const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+  });
+ 
+  const Person = mongoose.model('Person', personSchema);
+ 
+  const person = new Person({
+    name: 'John',
+    age: 37
+  });
+ 
+  //await person.save();
+ 
+  const people = await Person.find();
+  console.log(people);
+ 
+  //const kiwi = new Fruit({
+   // name: 'Kiwi',
+  //  score: 10,
+ //   review: 'The best fruit!'
+ // });
+ 
+  //const orange = new Fruit({
+  //  name: 'Orange',
+ //   score: 4,
+ //   review: 'Too sour for me'
+  //});
+ 
+  //const banana = new Fruit({
+//    name: 'Banana',
+  //  score: 3,
+  //  review: 'Weird texture'
+ // });
+ 
+ 
+ 
+  //Fruit.insertMany([kiwi, orange, banana])
+ // .then(function () {
+  //  console.log("Successfully saved defult items to DB");
+ // })
+//  .catch(function (err) {
+ //   console.log(err);
+ // });
+ 
+} 
